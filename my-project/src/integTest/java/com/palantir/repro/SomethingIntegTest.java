046f499315d11eb36fb5773c5369d5f9a7a9e65d
Dan Fox <dfox@palantir.com>
2017-06-20 14:31:24 +0100

    SomethingTest and SomethingIntegTest

---
+++ my-project/src/integTest/java/com/palantir/repro/SomethingIntegTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ */
+
+package com.palantir.repro;
+
+import org.junit.Test;
+
+public class SomethingIntegTest {
+
+    @Test
+    public void integ_test() {
+        System.out.println("This passes in my IDE");
+    }
+}
---
+++ my-project/src/test/java/com/palantir/repro/SomethingTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ */
+
+package com.palantir.repro;
+
+import org.junit.Test;
+
+public class SomethingTest {
+
+    @Test
+    public void unit_test() {
+        System.out.println("This passes in my IDE");
+    }
+}
